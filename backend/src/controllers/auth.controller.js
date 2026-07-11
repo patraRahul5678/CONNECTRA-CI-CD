4b83a6f40c1a65a23cd5772616f8c4de4dd3d83e
patraRahul5678 <kanhapatra801@gmail.com>
2026-07-11 13:36:03 +0530

    Changes made in the controller of the github

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -99,7 +99,7 @@ export async function login(req, res) {
             secure: process.env.NODE_ENV === "production" //cookie will only be sent over HTTPS in production
         });
         res.status(201).json({ success: true, user });
-        console.log(`User ${user.fullName} logged in successfully....`);
+        console.log(`User ${user.fullName} logged in successfully to Connectra....`);
 
     } catch (error) {
         console.error("Error in login Controller:", error);
